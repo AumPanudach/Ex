b6671fb5b81dfe90dee8db2f8e3cf62380d9cef8
Panudach Kongsomboon <120073076+AumPanudach@users.noreply.github.com>
2023-10-25 11:07:22 +0700

    models

---
+++ School.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class School extends Model
+{
+    use HasFactory;
+    protected $table = "school";
+    public function newMethodName(){
+        return $this->belongsTo('App\Models\City','city_id');
+    }
+}
